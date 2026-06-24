3940021832f8d32aed3fe6e6156d1d681856d2bd
jakkr01 <100818063+jakkr01@users.noreply.github.com>
2026-06-24 16:34:56 +0700

    ส่งงาน Workshop 2_1

---
+++ Movie.ts
@@ -0,0 +1,14 @@
+let age: number = 6;
+
+if (age < 12) {
+    console.log(`Age: ${age}`);
+    console.log("Price is $5.");
+}
+else if (age >= 12 && age <= 60) {
+    console.log(`Age: ${age}`);
+    console.log("Price is $10.");
+}
+else {
+    console.log(`Age: ${age}`);
+    console.log("Price is $7.");
+}
